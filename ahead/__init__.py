1954ed46741be82e913d11c8cdc341f3f09fec7b
thierrymoudiki <thierry.moudiki@gmail.com>
2021-12-09 17:21:04 +0100

    update to v0.3.0

--- ahead/__init__.py
+++ ahead/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """T. Moudiki"""
 __email__ = 'thierry.moudiki@gmail.com'
-__version__ = '0.2.4'
+__version__ = '0.3.0'
 
 from .DynamicRegressor import DynamicRegressor
 from .EAT import EAT
