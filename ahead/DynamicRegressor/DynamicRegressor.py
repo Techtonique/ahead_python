ae6c1c894fef0316d6c4303048265b8a4391ae45
thierrymoudiki <thierry.moudiki@gmail.com>
2021-12-11 10:59:52 +0100

    do docstrings for class DynamicRegressor

--- ahead/DynamicRegressor/DynamicRegressor.py
+++ ahead/DynamicRegressor/DynamicRegressor.py
@@ -35,6 +35,61 @@ stats = importr('stats')
 ahead = importr('ahead')
 
 class DynamicRegressor():
+    """ Dynamic Regression Model adapted from R's `forecast::nnetar`
+
+    Parameters:
+       
+        h: an integer;
+            forecasting horizon
+        
+        level: an integer;
+            Confidence level for prediction intervals
+
+        type_pi: a string;
+            Type of prediction interval (currently "gaussian", 
+            ETS: "E", Arima: "A" or Theta: "T")
+
+        date_formatting: a string;
+            Currently: 
+            - "original": yyyy-mm-dd
+            - "ms": milliseconds        
+
+    Attributes:
+       
+        fcast_: an object;
+            raw result from fitting R's `ahead.dynrmf`
+        
+        averages_: a list;
+            mean forecast in a list
+
+        ranges_: a list;
+            lower and upper prediction intervals in a list
+
+        output_dates_: a list;
+            a list of output dates (associated to forecast)
+
+        result_df_: a data frame;
+            contains 3 columns, mean forecast, lower + upper 
+            prediction intervals, and a date index          
+
+    Examples:
+       
+        ```
+            # Data frame containing the time series 
+            dataset = {
+            'date' : ['2020-01-01', '2020-02-01', '2020-03-01', '2020-04-01', '2020-05-01'],
+            'value' : [34, 30, 35.6, 33.3, 38.1]}
+
+            df = pd.DataFrame(dataset).set_index('date')
+            print(df)
+
+            # univariate time series forecasting 
+            d1 = DynamicRegressor(h = 5)
+            d1.forecast(df)
+            print(d1.result_df_)
+        ```
+
+    """
 
     def __init__(self, h=5, level=95,                  
                  type_pi="E", date_formatting="original"):
@@ -50,7 +105,15 @@ class DynamicRegressor():
         self.output_dates_ = []      
         self.result_df_ = None
 
-    def forecast(self, df):            
+    def forecast(self, df):   
+        """ Forecasting method from `DynamicRegressor` class
+
+        Parameters:
+       
+            df: a data frame;
+                a data frame containing the time series (see example)                
+
+        """         
         
         self.input_df = df
 
