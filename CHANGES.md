2b134f25674677329c343200d0471c6c85ec040c
thierrymoudiki <thierry.moudiki@gmail.com>
2021-12-27 20:32:45 +0100

    upgrade version

--- CHANGES.md
+++ CHANGES.md
@@ -1,3 +1,4 @@
 # version 0.4.1
 
 - use `get_frequency` to get series frequency as a number
+- create a function `get_tscv_indices` for getting time series cross-validation indices
--- ahead/__init__.py
+++ ahead/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """T. Moudiki"""
 __email__ = "thierry.moudiki@gmail.com"
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 
 from .DynamicRegressor import DynamicRegressor
 from .EAT import EAT
