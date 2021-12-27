97c180a6afc355ed9d2ca31694fb68fedffc37a9
thierrymoudiki <thierry.moudiki@gmail.com>
2021-12-27 18:37:23 +0100

    create function for time series splitting Pt.2

--- ahead/utils/__init__.py
+++ ahead/utils/__init__.py
@@ -1,6 +1,7 @@
 from .unimultivariate import compute_output_dates, compute_result_df, get_frequency
 from .univariate import compute_y_ts, format_univariate_forecast
 from .multivariate import compute_y_mts, format_multivariate_forecast
+from .tscv_indices import get_tscv_indices
 
 __all__ = [
     "compute_output_dates",
@@ -9,5 +10,6 @@ __all__ = [
     "compute_y_mts",
     "format_multivariate_forecast",
     "compute_result_df",
-    "get_frequency"
+    "get_frequency",
+    "get_tscv_indices"
 ]
