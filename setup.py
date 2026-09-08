da245f0f1623de97d6f77f1bc847db798b24a98b
Thierry Moudiki <thierry.moudiki@gmail.com>
2026-09-08 08:50:42 +0200

    Bump version from 0.38.5 to 0.38.6

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ class DevelopCommand(develop):
         develop.run(self)
 
 
-__version__ = "0.38.5"
+__version__ = "0.38.6"
 
 with open("requirements.txt", encoding="utf-8") as f:
     all_reqs = f.read().split("\n")
